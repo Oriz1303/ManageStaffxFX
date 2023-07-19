f393f346b72083b181173aa290b61a8e456b9e42
oriz <oriz1303@gmail.com>
2023-07-19 08:55:10 +0700

    add file config properties

--- src/main/java/com/example/managestaff/dao/JDBCConnect.java
+++ src/main/java/com/example/managestaff/dao/JDBCConnect.java
@@ -1,17 +1,29 @@
 package com.example.managestaff.dao;
 
-import com.example.managestaff.config.IDBConfig;
-
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.*;
+import java.util.Properties;
 
 public class JDBCConnect {
     public boolean login(String username, String password) {
         String query = "SELECT * FROM account WHERE username = ? AND password = ?;";
-        try (Connection connect = DriverManager.getConnection(IDBConfig.DB_URL, IDBConfig.USERNAME, IDBConfig.PASSWORD);
+//        ResourceBundle rb = ResourceBundle.getBundle("config/DBConfig.properties");
+        Properties properties = new Properties();
+
+        try (InputStream inputStream = getClass().getResourceAsStream("/config/DBConfig.properties")) {
+            properties.load(inputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        try (Connection connect = DriverManager.getConnection(properties.getProperty("DB_URL"), properties.getProperty("DB_USERNAME"), properties.getProperty("DB_PASSWORD"));
              PreparedStatement ps = connect.prepareStatement(query);
         ) {
             ps.setString(1, username);
             ps.setString(2, password);
+            System.out.println(properties.getProperty("DB_URL"));
 
             ResultSet resultSet = ps.executeQuery();
             if (resultSet.next()) {
@@ -23,3 +35,32 @@ public class JDBCConnect {
         return false;
     }
 }
+
+
+
+//public class Main extends Application {
+//    @Override
+//    public void start(Stage primaryStage) {
+//        // Load the properties file
+//        Properties properties = new Properties();
+//        try (InputStream inputStream = getClass().getClassLoader().getResourceAsStream("config.properties")) {
+//            properties.load(inputStream);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//
+//        // Retrieve values from the properties file
+//        String applicationName = properties.getProperty("application.name");
+//        String applicationVersion = properties.getProperty("application.version");
+//
+//        System.out.println("Application Name: " + applicationName);
+//        System.out.println("Application Version: " + applicationVersion);
+//
+//        // Rest of your JavaFX code...
+//    }
+//
+//    public static void main(String[] args) {
+//        launch(args);
+//    }
+//}
+
